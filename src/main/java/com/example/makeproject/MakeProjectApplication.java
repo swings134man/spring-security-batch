eea8b7804338fa102648110005b9f906731f0c72
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-12-27 20:55:40 +0900

    first batch file

--- src/main/java/com/example/makeproject/MakeProjectApplication.java
+++ src/main/java/com/example/makeproject/MakeProjectApplication.java
@@ -1,10 +1,12 @@
 package com.example.makeproject;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableBatchProcessing // Batch use
 //@SpringBootApplication
 public class MakeProjectApplication {
 
