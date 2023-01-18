31e6c3bdce123c7c7c2bc27d2ec69188c2900746
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-01-18 17:58:36 +0900

    Swagger-ui 적용 & Test 완료

--- src/main/java/com/example/makeproject/board/controller/BoardController.java
+++ src/main/java/com/example/makeproject/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.makeproject.board.controller;
 
 import com.example.makeproject.board.domain.BoardDTO;
 import com.example.makeproject.board.service.BoardService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class BoardController {
 
     private final BoardService service;
 
+    @ApiOperation(value = "게시판 작성 api", notes = "게시판글 작성시 저장한다.")
     @PostMapping("/api/savePost")
     public BoardDTO savePost(@RequestBody BoardDTO inDTO) {
 //        BoardDTO dto = BoardDTO.builder()
@@ -24,15 +26,13 @@ public class BoardController {
 //                .content("content1")
 //                .writer("writer1")
 //                .build();
-
         BoardDTO result = service.savePost2(inDTO);
         return result;
     }
 
+    @ApiOperation(value = "게시판 전체 조회 api", notes = "조건없이 게시판글 전체 조회한다.(List<BoardDTO>)")
     @GetMapping("/api/findAll")
     public List<BoardDTO> findAll() {
         return service.findAll();
     }
-
-
 }
