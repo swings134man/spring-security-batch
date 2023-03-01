74adafee63ed0853f5a102a3a426fd823af275a7
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-03-02 02:24:33 +0900

    Scope File ADD

---
+++ src/main/java/com/example/makeproject/batch/domain_detail/scope/Scope_config_1.java
@@ -0,0 +1,16 @@
+package com.example.makeproject.batch.domain_detail.scope;
+
+/************
+ * @info : Batch - Scope 예제 conifg class
+ * @name : Scope_config_1
+ * @date : 2023/03/02 2:23 AM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description :
+ *
+ * - Scope 란 : Spring Container에서 관리되는 Bean의 범위
+ * -
+ *
+ ************/
+public class Scope_config_1 {
+}
