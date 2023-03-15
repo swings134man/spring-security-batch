40bdb246bf34771b71b4ce143d2bcfed23c94f70
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-03-16 02:25:30 +0900

    Spring 인식불가 commit 시점

---
+++ src/main/java/com/example/makeproject/batch/practice/test/JobTestConfig.java
@@ -0,0 +1,14 @@
+package com.example.makeproject.batch.practice.test;
+
+import org.springframework.context.annotation.Configuration;
+
+/************
+ * @info : Batch - Test
+ * @name : JobTestConfig
+ * @date : 2023/03/16 2:19 AM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description :
+ ************/
+public class JobTestConfig {
+}
