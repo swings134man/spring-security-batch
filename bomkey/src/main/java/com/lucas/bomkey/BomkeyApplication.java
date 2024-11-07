1349d7333cd967c4cbec0692c90bccc68e393753
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-11-07 17:09:24 +0900

    feature: Auth Server Security Config 기본

--- bomkey/src/main/java/com/lucas/bomkey/BomkeyApplication.java
+++ bomkey/src/main/java/com/lucas/bomkey/BomkeyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // FIXME: Test Only exclude
+@SpringBootApplication
 public class BomkeyApplication {
 
 	public static void main(String[] args) {
---
+++ bomkey/src/main/java/com/lucas/bomkey/rest/TestController.java
@@ -0,0 +1,17 @@
+package com.lucas.bomkey.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test Success";
+    }
+}
